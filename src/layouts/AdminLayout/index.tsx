893cce7467ea43d6b90bc89de47ef3d629d914bf
hanchi <hanchi@tosky.co.kr>
2024-05-02 13:34:19 +0900

    feat: admin 페이지에도 로딩 추가

--- src/layouts/AdminLayout/index.tsx
+++ src/layouts/AdminLayout/index.tsx
@@ -47,83 +47,91 @@ export default function AdminLayout() {
 
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
-      <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-neutral-800 sm:flex text-neutral-400">
-        <TooltipProvider>
-          <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
-            <SideTooltip
-              title="Dashboard"
-              link="/admin"
-              icon={<Home className="h-5 w-5" />}
-            />
-            <SideTooltip
-              title="Subjects"
-              link="/admin/subjects"
-              icon={<Book className="h-5 w-5" />}
-            />
-            <SideTooltip
-              title="Questions"
-              link="/admin/questions"
-              icon={<MessageCircleQuestion className="h-5 w-5" />}
-            />
-          </nav>
-        </TooltipProvider>
-      </aside>
-      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14 min-h-screen bg-neutral-200">
-        <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 justify-between">
-          <Breadcrumb className="hidden md:flex">
-            <BreadcrumbList>
-              <BreadcrumbItem>
-                <BreadcrumbLink asChild>
-                  <Link to="#">Dashboard</Link>
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator />
-              <BreadcrumbItem>
-                <BreadcrumbLink asChild>
-                  <Link to="#">Products</Link>
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator />
-              <BreadcrumbItem>
-                <BreadcrumbPage>All Products</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button
-                variant="outline"
-                size="icon"
-                className="overflow-hidden rounded-full"
-              >
-                <Avatar>
-                  <AvatarImage
-                    src="/placeholder-user.jpg"
-                    className="h-9 w-9"
-                  />
-                </Avatar>
-                <img
-                  src="/placeholder-user.jpg"
-                  width={36}
-                  height={36}
-                  alt="Avatar"
-                  className="overflow-hidden rounded-full"
+      {isLoading ? (
+        <div className="flex items-center justify-center h-screen">
+          <span className="text-2xl font-bold">Loading...</span>
+        </div>
+      ) : (
+        <>
+          <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-neutral-800 sm:flex text-neutral-400">
+            <TooltipProvider>
+              <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
+                <SideTooltip
+                  title="Dashboard"
+                  link="/admin"
+                  icon={<Home className="h-5 w-5" />}
                 />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Settings</DropdownMenuItem>
-              <DropdownMenuItem>Support</DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Logout</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </header>
-        <Outlet />
-      </div>
+                <SideTooltip
+                  title="Subjects"
+                  link="/admin/subjects"
+                  icon={<Book className="h-5 w-5" />}
+                />
+                <SideTooltip
+                  title="Questions"
+                  link="/admin/questions"
+                  icon={<MessageCircleQuestion className="h-5 w-5" />}
+                />
+              </nav>
+            </TooltipProvider>
+          </aside>
+          <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14 min-h-screen bg-neutral-200">
+            <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 justify-between">
+              <Breadcrumb className="hidden md:flex">
+                <BreadcrumbList>
+                  <BreadcrumbItem>
+                    <BreadcrumbLink asChild>
+                      <Link to="#">Dashboard</Link>
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem>
+                    <BreadcrumbLink asChild>
+                      <Link to="#">Products</Link>
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>All Products</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className="overflow-hidden rounded-full"
+                  >
+                    <Avatar>
+                      <AvatarImage
+                        src="/placeholder-user.jpg"
+                        className="h-9 w-9"
+                      />
+                    </Avatar>
+                    <img
+                      src="/placeholder-user.jpg"
+                      width={36}
+                      height={36}
+                      alt="Avatar"
+                      className="overflow-hidden rounded-full"
+                    />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>Settings</DropdownMenuItem>
+                  <DropdownMenuItem>Support</DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>Logout</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </header>
+            <Outlet />
+          </div>
+        </>
+      )}
     </div>
   );
 }
